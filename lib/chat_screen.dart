0b2589e5585e06fd962bfd9e1b230f78ded92b2d
Alan Cristian <lanzn16@outlook.com>
2020-07-29 23:10:12 -0300

    add sign in authentication with google acc

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -2,39 +2,95 @@ import 'dart:io';
 
 import 'package:chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
-void _sendMessage({String text, File imgFile}) async {
-  Map<String, dynamic> data = {};
+class _ChatScreenState extends State<ChatScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
 
-  if (imgFile != null) {
-    StorageUploadTask task = FirebaseStorage.instance
-        .ref()
-        .child(DateTime.now().millisecondsSinceEpoch.toString())
-        .putFile(imgFile);
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-    StorageTaskSnapshot taskSnapshot = await task.onComplete;
+  FirebaseUser _currentUser;
 
-    String url = await taskSnapshot.ref.getDownloadURL();
+  @override
+  void initState() {
+    super.initState();
 
-    data["imgUrl"] = url;
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
   }
 
-  if (text != null) data["text"] = text;
+  Future<FirebaseUser> _getUser() async {
+    if (_currentUser != null) return _currentUser;
 
-  Firestore.instance.collection("messages").add(data);
-}
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
 
-class _ChatScreenState extends State<ChatScreen> {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+
+      final AuthResult authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  void _sendMessage({String text, File imgFile}) async {
+    final FirebaseUser user = await _getUser();
+
+    if (user == null) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text("unable to sign in. try again!"),
+        backgroundColor: Colors.red,
+      ));
+    }
+
+    Map<String, dynamic> data = {
+      "uid": user.uid,
+      "senderName": user.displayName,
+      "senderPhotoUrl": user.photoUrl,
+    };
+
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
+
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+
+      String url = await taskSnapshot.ref.getDownloadURL();
+
+      data["imgUrl"] = url;
+    }
+
+    if (text != null) data["text"] = text;
+
+    Firestore.instance.collection("messages").add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Olá"),
         elevation: 0,
